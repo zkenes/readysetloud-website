0c1e09886ec0bdaf64e11335109e56d3b63f50e8
Zhanserik Kenes <zhans.kenes@gmail.com>
2017-07-26 14:05:24 +0600

    chore: change credentials

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_db');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'zkenes');
+define('DB_USER', 'sound_zkenes');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'passwordhere');
+define('DB_PASSWORD', 'Apple1993');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
